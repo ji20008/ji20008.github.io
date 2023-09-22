67235836fdc662c538983a746ee2535e0ee9b635
James Irving <144828519+ji20008@users.noreply.github.com>
2023-09-22 09:47:05 +0100

    Update javascriptTest.js

--- javascriptTest.js
+++ javascriptTest.js
@@ -1,8 +1,34 @@
+/*file to test javascript basics */
+
+
+//variables
 var a = 24;
+
+//if else statement using Nan
 if (isNaN(a)){
     alert("a is not a number");
 }else{
     alert("a is number of value" + a);
 
 }
+
+//for loop
+
+
+
+//while loop
+
+
+
+//simple function
+
+
+
+
+//simple array
+
+
+
+
+//create objects
 
