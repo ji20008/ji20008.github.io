cfddb0bc7d8733dd36cb8074621dadf6c8eb4af2
James Irving <144828519+ji20008@users.noreply.github.com>
2023-09-28 17:36:41 +0100

    renamed files and added code
    adding the cookie banner javascript

---
+++ Lab Excercises/week3lab/cookiebanner.js
@@ -0,0 +1,9 @@
+// node.className = 'test';
+// node.framebroder = '0';
+// node['data-message']='0';
+// node.setAttribute('data-message', 'Test');
+
+document.body.style.height = '200pt';
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
