6232a946320866967c581b2a5ee9d79449b22762
James Irving <144828519+ji20008@users.noreply.github.com>
2023-11-07 21:25:32 +0000

    added another form that uses web storage

--- websiteFiles/CodeFiles/webstorage.js
+++ websiteFiles/CodeFiles/webstorage.js
@@ -10,3 +10,20 @@ if(localStorage){//check if local storage object exists
 }else {
     alert("Sorry your browser does not support local storage");
 }
+
+function savewb2() {
+    var key = document.getElementById("sKey").value;
+    if ( key != "" )
+        sessionStorage[key] = document.getElementById("sValue").value;
+}
+function accesswb2() {
+    var s = "";
+    for (var i=0; i<sessionStorage.length; i++) {
+        var key = sessionStorage.key(i);
+        s += key + " = " + sessionStorage.getItem(key) + "\n";
+    }
+    alert( s );
+}
+function clearwb() {
+    sessionStorage.clear();
+}
