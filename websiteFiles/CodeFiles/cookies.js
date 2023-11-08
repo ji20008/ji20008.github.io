d3835c8387d98542f9a9d317534ec4f60dfb6e09
James Irving <144828519+ji20008@users.noreply.github.com>
2023-11-08 22:27:12 +0000

    everything implemented, code saves a username with cookies. and uses cookies to get and display the username

--- websiteFiles/CodeFiles/cookies.js
+++ websiteFiles/CodeFiles/cookies.js
@@ -2,7 +2,7 @@
 function setCookie(){
     const username = document.getElementById("username").value;
     //set a cookie with the collected username, with attributes
-    document.cookie = 'username=${username}; expires = Fri, 5 Jul 2024 23:59:59 GMT; path=/';
+    document.cookie = `username=${username}; expires = Fri, 5 Jul 2024 23:59:59 GMT; path=/`;
     alert("Username cookie set!");
 
 }
@@ -11,7 +11,7 @@ function setCookie(){
 function getCookie(name){
     const cookies = document.cookie.split('; ');
     for (let i=0; i<cookies.length; i++){
-        const cookie = cookie[i].split('=');
+        const cookie = cookies[i].split('=');
         if (cookie[0] === name){
             return cookie[1];
         }
