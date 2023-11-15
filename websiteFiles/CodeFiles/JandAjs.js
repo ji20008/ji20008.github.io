445231c98f76dd9e05cffc50b960e50efe6e7e2d
James Irving <144828519+ji20008@users.noreply.github.com>
2023-11-15 18:56:49 +0000

    added the fetch section to the javascript

--- websiteFiles/CodeFiles/JandAjs.js
+++ websiteFiles/CodeFiles/JandAjs.js
@@ -7,6 +7,25 @@ document.getElementById("loginForm").addEventListener("submit", function(event){
     const password = document.getElementById("password").value;
 
     //Fetch json data and check login details
-    
+    fetch("JandAusers.json")
+        .then((response) => response.json())
+        .then((data) => {
+            //Extract the array of users from the json data
+            const users = data.users;
+
+            //Find a user that matches the entered details
+            const user = users.find((u) => u.username === username && u.password === password);
+
+            //Display a message based on the login result
+            if(user){
+                document.getElementById("message").textContent = "Login successful!";
+            }else{
+                document.getElementById("message").textContent = "Invalid username or password";
+            }
+        })
+        .catch((error)=>{
+            //Log an error if theres an issue fetching the json data
+            console.error("Error fetching data: ",error);
+        })
 
 });
